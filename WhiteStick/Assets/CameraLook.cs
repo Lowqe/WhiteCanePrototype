ddb45072c4f2e6c010373c85e701fba564bda096
Lowqe <Lowqe@users.noreply.github.com>
2020-01-28 15:10:40 +0100

    Projektet
    Det går att gå, röra runt en pinne och hålla in en knapp för att styra kameran

---
+++ WhiteStick/Assets/CameraLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform cameraRotation;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            //yRotation += mouseX;
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            cameraRotation.Rotate(Vector3.up * mouseX);
+        }
+
+        
+    }
+    
+}
---
+++ WhiteStick/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform stickRotation;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (!Input.GetKey(KeyCode.Mouse0))
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            yRotation += mouseX;
+
+            transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+            
+        }
+
+    }
+}
